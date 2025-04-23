549f44fd4d4c7a607f460324bb56a4dee9267efd
Mujtaba Qureshi <mujtabaqureshi009@gmail.com>
2025-04-23 13:46:54 +0500

    Update Main code.cpp

--- Main code.cpp
+++ Main code.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <random>
 #include <iostream>
+using namespace std;
 
 #define SIZE 30
 #define CELL_WIDTH 20
@@ -13,7 +14,7 @@ public:
     int pos = 0;
     float size = CELL_WIDTH;
     float thickness = 2.f;
-    bool walls[4] = { true, true, true, true }; // top, right, bottom, left
+    bool walls[4] = { true, true, true, true };
     bool visited = false;
     bool isActive = false;
 
@@ -32,22 +33,22 @@ public:
 
         rect.setFillColor(sf::Color(223, 243, 228));
 
-        if (walls[0]) { // top
+        if (walls[0]) {
             rect.setSize({ size, thickness });
             rect.setPosition(x, y);
             window.draw(rect);
         }
-        if (walls[1]) { // right
+        if (walls[1]) {
             rect.setSize({ thickness, size });
             rect.setPosition(x + size, y);
             window.draw(rect);
         }
-        if (walls[2]) { // bottom
+        if (walls[2]) {
             rect.setSize({ size + thickness, thickness });
             rect.setPosition(x, y + size);
             window.draw(rect);
         }
-        if (walls[3]) { // left
+        if (walls[3]) {
             rect.setSize({ thickness, size });
             rect.setPosition(x, y);
             window.draw(rect);
@@ -139,25 +140,112 @@ public:
     int getSize() const { return size; }
 };
 
+class Lobby {
+private:
+    sf::RenderWindow &window;
+    sf::Font font;
+    sf::Text title;
+    sf::Text start;
+    sf::Text exit;
+    bool startgame;
+
+
+            
+
+public:
+
+    Lobby(sf::RenderWindow &window):window(window) {
+        startgame = false;
+        cout << "Welcome to the Maze Game! " << endl;;
+        cout << "Use arrow keys or HJKL to navigate"<<endl;
+        cout << "Reach the bottom right corner to win! "<<endl;
+
+
+		if (!font.loadFromFile("Data/Roboto.ttf")) {
+			std::cout << "Error loading font" << std::endl;
+		}
+		title.setFont(font);
+		title.setString("Maze Game");
+		title.setCharacterSize(50);
+		title.setFillColor(sf::Color::White);
+		title.setPosition(window.getSize().x/2 -title.getGlobalBounds().width/2,100);
+
+		start.setFont(font);
+		start.setString("Press Enter to Start");
+		start.setCharacterSize(30);
+		start.setFillColor(sf::Color::White);
+		start.setPosition(window.getSize().x/2-start.getGlobalBounds().width/2,300);
+
+		exit.setFont(font);
+		exit.setString("Press Escape to Exit");
+		exit.setCharacterSize(30);
+		exit.setFillColor(sf::Color::White);
+		exit.setPosition(window.getSize().x/2-exit.getGlobalBounds().width/2, 400);
+
+
+
+
+
+    }   
+	bool run() {
+
+        while (window.isOpen()) {
+            handlevent();
+            render();
+            if (startgame) {
+			
+                return true;
+            }
+           
+        }
+        return false;
+	
+	}
+
+	void handlevent() {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Enter) {
+					startgame = true;
+				}
+				else if (event.key.code == sf::Keyboard::Escape) {
+					window.close();
+				}
+			}
+		}
+	}
+
+    void render() {
+		window.clear(sf::Color(13, 2, 33));
+		window.draw(title);
+		window.draw(start);
+		window.draw(exit);
+		window.display();
+
+    }
+};
+
 class Game {
 private:
-
     sf::Clock clock;
     sf::Time elapsedTime;
     sf::Font font;
     sf::Text timerText;
+    sf::Clock freezeClock;
 
-   
-    int bestTime = -1; // -1 means no best time yet
-    int countdownSeconds = 60; // 60-second countdown
+    int bestTime = -1;
+    int countdownSeconds = 60;
     bool countdownExpired = false;
+    bool showingGameOver = false;
 
-    
     sf::Text bestTimeText;
     sf::Text countdownText;
+    sf::Text gameOverText;
 
-
-    sf::RenderWindow window;
+    sf::RenderWindow &window;
     Maze maze;
     int currentPos = 0;
 
@@ -165,17 +253,24 @@ private:
     sf::RectangleShape goalHighlight;
 
 public:
-    Game() : window(sf::VideoMode(CELL_WIDTH* SIZE + 60, CELL_WIDTH* SIZE + 60), "OOP Maze"),
+    Game(sf::RenderWindow &win) : window(win),
         maze(SIZE) {
 
-		if (!font.loadFromFile("Data/Roboto.ttf")) {
-			std::cout << "Error loading font" << std::endl;
-		}
+        if (!font.loadFromFile("Data/Roboto.ttf")) {
+            std::cout << "Error loading font" << std::endl;
+        }
+
+        gameOverText.setFont(font);
+        gameOverText.setString("Game Over");
+        gameOverText.setCharacterSize(60);
+        gameOverText.setFillColor(sf::Color::Red);
+        gameOverText.setStyle(sf::Text::Bold);
+        gameOverText.setPosition(200, 200);
 
         timerText.setFont(font);
         timerText.setCharacterSize(20);
         timerText.setFillColor(sf::Color::White);
-        timerText.setPosition(30, 5); // Position near top-left
+        timerText.setPosition(30, 5);
 
         bestTimeText.setFont(font);
         bestTimeText.setCharacterSize(20);
@@ -187,10 +282,7 @@ public:
         countdownText.setFillColor(sf::Color::Red);
         countdownText.setPosition(300, 5);
 
-
        
-
-        window.setFramerateLimit(30);
         maze.getCell(currentPos).isActive = true;
 
         currentHighlight.setSize({ CELL_WIDTH, CELL_WIDTH });
@@ -215,7 +307,7 @@ private:
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            if (event.type == sf::Event::KeyPressed)
+            if (event.type == sf::Event::KeyPressed && !showingGameOver)
                 handleMovement(event.key.code);
         }
     }
@@ -238,18 +330,27 @@ private:
     }
 
     void update() {
+        if (showingGameOver) {
+            if (freezeClock.getElapsedTime().asSeconds() > 3.0f) {
+                restartGame();
+            }
+            return;
+        }
+
         if (currentPos == SIZE * SIZE - 1) {
-            maze.generateMaze();
-            currentPos = 0;
-            maze.getCell(currentPos).isActive = true;
-            clock.restart();
+            int elapsed = static_cast<int>(clock.getElapsedTime().asSeconds());
+
+            if (bestTime == -1 || elapsed < bestTime)
+                bestTime = elapsed;
+
+            restartGame();
         }
 
         elapsedTime = clock.getElapsedTime();
         int seconds = static_cast<int>(elapsedTime.asSeconds());
-		int remainingTime = countdownSeconds - seconds;
-        timerText.setString("Time: " + std::to_string(seconds) + "s");
+        int remainingTime = countdownSeconds - seconds;
 
+        timerText.setString("Time: " + std::to_string(seconds) + "s");
 
         if (bestTime != -1)
             bestTimeText.setString("Best: " + std::to_string(bestTime) + "s");
@@ -261,45 +362,51 @@ private:
         else {
             countdownText.setString("Time's up!");
             countdownExpired = true;
+            showingGameOver = true;
+            freezeClock.restart();
         }
-        if (currentPos == SIZE * SIZE - 1) {
-            int elapsed = static_cast<int>(clock.getElapsedTime().asSeconds());
-
-            if (bestTime == -1 || elapsed < bestTime)
-                bestTime = elapsed;
-
-            maze.generateMaze();
-            currentPos = 0;
-            clock.restart();
-            countdownExpired = false;
-            maze.getCell(currentPos).isActive = true;
-        }
-		if (countdownExpired) {
-			countdownText.setString("Time's up!");
-			countdownText.setFillColor(sf::Color::Red);
-		}
-		else {
-			countdownText.setFillColor(sf::Color::Red);
-		}
-
     }
 
     void render() {
         window.clear(sf::Color(13, 2, 33));
         maze.draw(window);
+
         currentHighlight.setPosition(maze.getCell(currentPos).x, maze.getCell(currentPos).y);
         window.draw(currentHighlight);
+
         goalHighlight.setPosition(maze.getCell(SIZE * SIZE - 1).x, maze.getCell(SIZE * SIZE - 1).y);
         window.draw(goalHighlight);
-		window.draw(timerText);
+
+        window.draw(timerText);
         window.draw(bestTimeText);
         window.draw(countdownText);
+
+        if (showingGameOver) {
+            window.draw(gameOverText);
+        }
+
         window.display();
+    }
+
+    void restartGame() {
+        maze.generateMaze();
+        currentPos = 0;
+        maze.getCell(currentPos).isActive = true;
+        clock.restart();
+        countdownExpired = false;
+        showingGameOver = false;
     }
 };
 
-int main() {
-    Game game;
-    game.run();
-    return 0;
+int main() {  
+sf::RenderWindow window(sf::VideoMode(CELL_WIDTH * SIZE + 60, CELL_WIDTH * SIZE + 60), "Maze Game");  
+   window.setFramerateLimit(60);  
+Lobby lobby(window);  
+if (lobby.run()) { 
+	Game game(window);  
+	game.run();  
+}  
+else {  
+	return 0;  
+}  
 }
